f7b9c99b1f4c5e776131363d228a18a39965a055
JasonLiang5129 <94155740+JasonLiang5129@users.noreply.github.com>
2021-11-11 22:18:11 -0800

    deleted unnecessary stuff

--- s5test/app/src/main/java/com/example/s5test/MainActivity.java
+++ s5test/app/src/main/java/com/example/s5test/MainActivity.java
@@ -12,54 +12,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        TextView textView = findViewById(R.id.textview);
-        // HELLOAWodihaiowdha
-
-
-
-        //asdfasldkfja;lskdfj;laskdjf
-        ///asdjflkasjk;dlfkajs;dlkf
-        //asdjfljasdl;kfjasl;dkfj
-        //asdlkfja;slkdfj
-
-        ///asdfas;dkfa;slkdfj;laskdjf;alskdjf;saf
-        ///asdfasd;fja;sldkfj
-//this is a change
-        // Peter change
-        //peter 2nd change
-
-        //peter 3rd change
-
-        //RNADNOM+AWDADWJ)AJD
-        //w dadoaihdiowhodahw
-<<<<<<< HEAD
-
-        // peter 4th change
-        // peter 5th change
-=======
-
-        // peter 4th change
-
->>>>>>> bdcf1524b689dcfe4ebfcf023006b1215f35d473
-
-        // oscar test change
-<<<<<<< HEAD
-        //alskdjf;alskdjasldkfj;alskdjf
-
-=======
-        // anmotehr nweadasdwa
->>>>>>> 1a41dddb4e0989895809ce6385eed325e272dd2b
-<<<<<<< HEAD
-
-            //askdjf;alsdkjfl;
-=======
-            // ANOWDHOAUHWDUA
->>>>>>> 6a22ee65b856da49da5a41f13c7ccc031681286e
-<<<<<<< HEAD
-
-            //jason change
-=======
-            //asd;flkajsd;lfkjasdf
->>>>>>> 3c6b009ca9f571f3de52f9afc5524eab54976713
     }
 }
