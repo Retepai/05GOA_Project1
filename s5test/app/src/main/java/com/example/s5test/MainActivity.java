c8d2127c5282a353c6f9bf26326294fb1d8a401c
Retepai <75108005+Retepai@users.noreply.github.com>
2021-11-12 15:28:51 -0800

    Update MainActivity.java

--- s5test/app/src/main/java/com/example/s5test/MainActivity.java
+++ s5test/app/src/main/java/com/example/s5test/MainActivity.java
@@ -11,7 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        //Peter has been here before
+        //hi
+        //hi
+        //hi
+        // this is a comment
+        /*this is also a comment*/
 
     }
 }
