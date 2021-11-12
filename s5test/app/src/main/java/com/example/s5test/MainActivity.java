bdcf1524b689dcfe4ebfcf023006b1215f35d473
oscryng <oscarboom214@gmail.com>
2021-11-11 22:03:14 -0800

    testChange1
    tst1

--- s5test/app/src/main/java/com/example/s5test/MainActivity.java
+++ s5test/app/src/main/java/com/example/s5test/MainActivity.java
@@ -32,12 +32,10 @@ public class MainActivity extends AppCompatActivity {
 
         //RNADNOM+AWDADWJ)AJD
         //w dadoaihdiowhodahw
-<<<<<<< HEAD
+
         // peter 4th change
-        
 
-=======
->>>>>>> 6f3515f4d97eb66f1e9085e155e409d4c93768e3
 
+        // oscar test change
     }
 }
